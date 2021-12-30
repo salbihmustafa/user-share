d2ac4f85f1330773165c1876fb7c71d703a3ca5b
Salbih Mustafa <salbih.mustafa@certegy.com>
2021-12-30 15:47:09 -0500

    updated styling of navlinks

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
 const MainNavigation = () => {
   return (
@@ -15,8 +16,8 @@ const MainNavigation = () => {
       <h1 className="main-navigation__title">
         <Link to="/">Title</Link>
       </h1>
-      <nav>
-        ...
+      <nav className="main-navigation__header-nav">
+        <NavLinks></NavLinks>
       </nav>
     </MainHeader>
   );
---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,24 @@
+import './NavLinks.css';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavLinks = (props) => {
+    return (
+        <ul className="nav-links">
+            <li>
+                <NavLink to="/" exact>ALL USERS</NavLink>
+            </li>
+            <li>
+                <NavLink to="/uid/places">MY PLACES</NavLink>
+            </li>
+            <li>
+                <NavLink to="/places/new">ADD PLACE</NavLink>
+            </li>
+            <li>
+                <NavLink to="/auth">AUTHENTICATE</NavLink>
+            </li>
+        </ul>
+    )
+}
+
+export default NavLinks;
