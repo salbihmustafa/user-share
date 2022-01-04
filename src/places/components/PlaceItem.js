0de168777f48f32414b9043aeab31c0e5e474bec
Salbih Mustafa <salbih.mustafa@certegy.com>
2022-01-04 17:37:37 -0500

    created modal and connected to map button

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,34 +1,60 @@
 import "./PlaceItem.css";
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/UIElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="place-item">
-      <Card className="place-card">
-        <div className="place-img">
-          <img src={props.imageUrl} alt={props.title}></img>
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>THE MAP!</h2>
         </div>
-        <div className="place-info">
-          <div>
-            <h2>{props.title}</h2>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-card">
+          <div className="place-img">
+            <img src={props.imageUrl} alt={props.title}></img>
           </div>
-          <div>
-            <h3>{props.address}</h3>
+          <div className="place-info">
+            <div>
+              <h2>{props.title}</h2>
+            </div>
+            <div>
+              <h3>{props.address}</h3>
+            </div>
+            <div>
+              <p>{props.description}</p>
+            </div>
           </div>
-          <div>
-            <p>{props.description}</p>
+          <hr />
+          <div className="place-btns">
+            <Button inverse onClick={openMapHandler}>
+              <i class="fas fa-map"></i>
+            </Button>
+            <Button inverse to={`/places/${props.id}`}>
+              <i class="fas fa-edit"></i>
+            </Button>
+            <Button danger>
+              <i class="fas fa-trash-alt"></i>
+            </Button>
           </div>
-        </div>
-        <hr />
-        <div className="place-btns">
-          <Button inverse><i class="fas fa-map"></i></Button>
-          <Button inverse to={`/places/${props.id}`}><i class="fas fa-edit"></i></Button>
-          <Button danger><i class="fas fa-trash-alt"></i></Button>
-        </div>
-      </Card>
-    </li>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
