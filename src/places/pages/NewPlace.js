b817fc64d52bba79867664e23ed8d90b9d473f5d
Salbih Mustafa <salbih.mustafa@certegy.com>
2022-01-07 15:04:51 -0500

    created input component and added validators

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import "./NewPlace.css";
+import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/components/util/validators";
 
 const NewPlace = () => {
-    return (
-        <div>New Place Page</div>
-    )
-}
+  return (
+    <form className="place-form">
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
+      <div></div>
+    </form>
+  );
+};
 
 export default NewPlace;
