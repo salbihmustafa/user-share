674783e938528424d8a0f082ca32313cd050b6ad
Salbih Mustafa <salbih.mustafa@certegy.com>
2022-01-09 17:18:12 -0500

    added form input handler with userReducer

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,34 +1,76 @@
 import "./NewPlace.css";
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/components/util/validators";
+import Button from "../../shared/components/UIElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/components/util/validators";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true; //helper variable
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }, //overwrite state with action
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
 const NewPlace = () => {
-  const titleInputHandler = useCallback((id, value, isValid) => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
     //to avoid infinite loop, useCallback is used with no dependencies
+    dispatch({type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id}); //goes to action
   }, []);
 
-  const descriptionInputHandler = useCallback((id, value, isValid) => {
-    //to avoid infinite loop, useCallback is used with no dependencies
-  }, []);
   return (
     <form className="place-form">
       <Input
         type="text"
         label="Title"
+        id="title"
         element="input"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
-        onInput={titleInputHandler}
+        onInput={inputHandler}
       />
       <Input
         label="Description"
+        id="description"
         element="textarea"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)"
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
       />
-      <div></div>
+      <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
     </form>
   );
 };
